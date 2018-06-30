048771bd4e7b77b1140dd904b1c0dcd44d479cf7
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-30 15:07:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,20 @@
 class Dog
 
+@@all = []
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all.each do |name|
+    puts Dog.name
+  end
+end
+
 
 def clear_all
   Dog.clear
